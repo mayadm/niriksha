5d2cf1e7cf836bfb884ba996ff413c341f69388b
freezmeinster <freez_meinster@yahoo.co.id>
2010-11-01 11:34:04 +0700

    rubah nama jadi niriksha

---
+++ system/application/controllers/admin.php
@@ -0,0 +1,20 @@
+<?php
+
+class Admin extends Controller {
+
+	function Admin()
+	{
+		parent::Controller();	
+	}
+	
+	function index()
+	{
+		echo "Hallo aku lib Melihatlihat";
+	}
+	
+	function search($location)
+	{
+		echo "Hallo aku lib Melihatlihat";
+	}
+	
+}
