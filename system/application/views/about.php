6b66559c19ab35950c70d53870f3a96c7e342dba
maya <anakkecilbaikhati@gmail.com>
2010-11-01 12:11:18 +0700

    about

--- system/application/views/about.php
+++ system/application/views/about.php
@@ -3,14 +3,14 @@
 			<div id="page-bgbtm">
 				<div id="content">
 					<div class="post">
-						<h2 class="title"><a href="#">Welcome to Niriksha  </a></h2>
+						<h2 class="title">Welcome to Niriksha</h2>
 						
 						<div style="clear: both;">&nbsp;</div>
 						<div class="entry">
-							<p>This is <strong>Niriksha</strong>, a web based application for video sharing managemen.</p> 
+							<p>This is <strong>Niriksha</strong>, a web based application for video sharing management.</p> 
 
 <p>Here you can see video surveillance live streaming, share video with others and create conference. </p>
-<p>This application made on Ubuntu 10.04, use PHP5, sqlite, flowplayer and VLC.</p>
+<p>This application made on Ubuntu 10.04, use code igniter, sqlite, flowplayer and VLC.</p>
 <p>You might email me to anakkecilbaikhati@gmail.com, if you want to know the script. </p>
 							
 						</div>
