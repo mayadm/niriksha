5dbcc0f9fe7e366e18878ef769117ed6b01977d8
maya <anakkecilbaikhati@gmail.com>
2010-11-01 11:59:57 +0700

    sdsdsfsdf

--- system/application/views/sidebar.php
+++ system/application/views/sidebar.php
@@ -2,27 +2,10 @@
 		<div id="sidebar">
 					<ul>
 						<li>
-							<div id="search" >
-								<form method="POST" action="
-								<?php echo site_url();?>/lib_melihatlihat/search/
-								<?php 
-								if ($now == ""){ echo "home";}
-								else echo $now;
-								?>
-								">
-									<div>
-										<input type="text" name="s" id="search-text" value="" />
-										<input type="submit" id="search-submit" value="GO" />
-									</div>
-								</form>
-							</div>
-							<div style="clear: both;">&nbsp;</div>
-						</li>
-						<li>
 						   <?php 
 						   if ($now == ""){
 						   $this->load->view("sidebar/home");
-						   }else if( $now == "login" || $now == "error"){ 
+						   }else if( $now == "about" || $now == "login" || $now == "error"){ 
                                                     echo "<p></p>";
                                                    } else $this->load->view("sidebar/$now");
 						   ?>
