27e0b70656ebda61c4b046fa77037228aa9c371e
freezmeinster <freez_meinster@yahoo.co.id>
2010-11-01 12:09:12 +0700

    about

--- system/application/views/about.php
+++ system/application/views/about.php
@@ -3,10 +3,10 @@
 			<div id="page-bgbtm">
 				<div id="content">
 					<div class="post">
-						<h2 class="title"><a href="#">Welcome to Niriksha  </a></h2>
+						<h2 class="title">Welcome to Niriksha</h2>
 						
 						<div style="clear: both;">&nbsp;</div>
-						<div class="entry">
+						<div class="entry" style="height:215px;">
 							<p>This is <strong>Niriksha</strong>, a web based application for video sharing managemen.</p> 
 
 <p>Here you can see video surveillance live streaming, share video with others and create conference. </p>
